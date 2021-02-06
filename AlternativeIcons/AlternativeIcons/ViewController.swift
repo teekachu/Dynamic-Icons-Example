74a42b2bb9f06b2153f26b3b7f3ae8285c627f8c
teekachu <tingbecker@gmail.com>
2021-02-05 19:05:46 -0800

    init

---
+++ AlternativeIcons/AlternativeIcons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlternativeIcons
+//
+//  Created by Ting Becker on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
